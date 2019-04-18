678803cd4ae6b3376b24815dc8b25b9d4695ac1b
Nicholas Freitas <nicholasfreitas@sympatico.ca>
2019-04-18 00:28:28 -0400

    Triangle Recursion Demonstration
    simple console app to demonstrate the power of  recursion using a small common comp sci problem with the Triangle values

---
+++ TriangleRecursionProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TriangleRecursionProblem
+{
+    class Program
+    {
+        //https://en.wikipedia.org/wiki/Triangular_number
+        /*The pythagorus numbers...
+         1,3,6,10,15,21,28,36 ...
+
+         Description:
+         We take the index of our position in the series and add the value of the previous index.
+             
+             */
+
+        static void Main(string[] args)
+        {
+            Triangle(1,0);
+        }
+
+        public static void Triangle(int currentIndex,int previousValue)
+        {
+            //I have my index
+            //I set up my previous value
+            //I add the previous value to the value of this index
+            int magicNumber = previousValue + currentIndex;
+            Console.WriteLine(magicNumber);
+
+            Triangle(++currentIndex, magicNumber);
+                                             
+        }
+
+    }
+}
